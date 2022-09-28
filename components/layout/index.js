85e95d7cfc8df2782333ff3dee6cebfec09eac75
Alex Lin <alex.lin9991@gmail.com>
2022-09-28 16:31:12 +1000

    background delay reduced to 1s

--- components/layout/index.js
+++ components/layout/index.js
@@ -10,7 +10,7 @@ const Layout = (props) => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 1.5, duration: 2 }}
+        transition={{ delay: 1, duration: 2 }}
         className="background"
       >
         <Image src={BackgroundImage} objectFit="cover" layout="fill"></Image>
