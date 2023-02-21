10d4bdaf47988b5a2e658808d2fb6bc7510a9bbc
Alex Lin <alex.lin9991@gmail.com>
2023-02-21 16:02:07 +1100

    make fonts preload

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,24 @@ const HomePage = () => {
           content="Portfolio site showcasing some of Alex's work"
         ></meta>
         <link rel="shortcut icon" href="/A.png" />
+        <link
+          rel="preload"
+          href="/fonts/GeneralSans-Medium.woff2"
+          as="font"
+          type="font/woff2"
+        />
+        <link
+          rel="preload"
+          href="/fonts/GeneralSans-Regular.woff2"
+          as="font"
+          type="font/woff2"
+        />
+        <link
+          rel="preload"
+          href="/fonts/GeneralSans-Semibold.woff2"
+          as="font"
+          type="font/woff2"
+        />
       </Head>
       <Layout>
         <Hero></Hero>
