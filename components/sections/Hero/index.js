bbd3f3d7eccddd808e88ce00725a7c490d4e4321
Alex Lin <alex.lin9991@gmail.com>
2023-07-20 09:56:46 +1000

    update hero

--- components/sections/Hero/index.js
+++ components/sections/Hero/index.js
@@ -1,55 +1,39 @@
 import Row from "../../layout/Row";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import HeroImage from "../../../assets/hero_pic.png";
 
 const Hero = () => {
   return (
     <div className="full-width-section text-center">
       <Row ss={1} es={7} sm={2} em={10} sl={3} el={13} sml={3} eml={9}>
         <div className="mt-48 relative">
-          {/* <motion.h1
-            initial={{ opacity: 0, x: 0 }}
-            animate={{ opacity: 0.05, x: "-1.9rem", y: "-1.9rem" }}
-            transition={{ duration: 0.6, delay: 0.6 }}
-            className="hidden md:inline absolute"
-          >
-            I am{" "}
-            <span className="text-transparent bg-gradient-to-r from-[#FFAA5B] to-highlight1 bg-clip-text">
-              Alex <span className="hidden xl:inline">Lin</span>
-            </span>
-          </motion.h1>
-          <motion.h1
-            initial={{ opacity: 0, x: 0 }}
-            animate={{ opacity: 0.05, x: "1.9rem", y: "1.9rem" }}
-            transition={{ duration: 0.6, delay: 0.6 }}
-            className="hidden md:inline absolute"
-          >
-            I am{" "}
-            <span className="text-transparent bg-gradient-to-r from-[#FFAA5B] to-highlight1 bg-clip-text">
-              Alex <span className="hidden xl:inline">Lin</span>
-            </span>
-          </motion.h1> */}
           <motion.h1
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6 }}
             className=""
           >
-            Hi, I{"'"}m{" "}
+            Hey, I{"'"}m{" "}
             <span className="text-transparent  bg-gradient-to-r from-[#FFAA5B] to-highlight1 bg-clip-text">
               Alex <span className="hidden xl:inline">Lin</span>
             </span>
           </motion.h1>
         </div>
       </Row>
-      <Row ss={1} es={7} sm={3} em={10} sl={3} el={13} sml={3} eml={9}>
+      <Row  ss={1} es={7} sm={3} em={9} sl={3} el={13} sml={3} eml={9}>
         <motion.h2
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          className="subtitle text-center text-grey mb-48 mt-2"
+          className="subtitle text-center text-grey mb-24 mt-8"
         >
-          An aspiring full-stack developer
+          I build <span className="text-transparent  bg-gradient-to-r from-[#FFAA5B] to-highlight1 bg-clip-text">human-centered
+            </span> applications
         </motion.h2>
+      </Row>
+      <Row ss={1} es={7} sm={3} em={9} sl={3} el={13} sml={3} eml={9} className={"mb-16"}>
+        <Image src={HeroImage} height={409} width={1040} alt="pic of me"/>
       </Row>
     </div>
   );
